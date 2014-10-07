177c5222c66c1874240e7eb432f835bfa5caec5f
Eric Randall <eric.randalls@gmail.com>
2014-10-07 16:48:58 -0400

    Initial Commit
    Initial day of code

---
+++ FareCheck.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import pylab as P
+import csv as csv
+
+of = pd.read_csv('oldfares.csv', header=None, index_col=None)
+nf = pd.read_csv('newfares.csv', header=None, index_col=None)
+of.columns = ['rt','o','d','fp','OLDur','OLDr','OLDeff','OLDdis','yn']
+nf.columns = ['rt','o','d','fp','ur','r','eff','dis','yn']
+
+def market(x):
+	if(x['o']<x['d']):
+		return x['o']+x['d']+x['fp']
+	else:
+		return x['d']+x['o']+x['fp']
+
+of['mkt'] = of.apply(market, axis=1)
+nf['mkt'] = nf.apply(market, axis=1)
+of = of.drop(['yn','o','d','rt','fp'], axis=1)
+nf = nf.drop(['yn'], axis=1)
+nf = pd.merge(nf, of, on='mkt')
+
+yofc = pd.DataFrame()
+def makeDF(x, fareplan):
+	if(x['fp']==fareplan):
+		yofc.append(x)
+		
+yofc = yofc.apply(makeDF(yofc), axis=1)
+print yofc.info()
+
+increase = float(raw_input('Enter percentage increase: '))
+nf['CALCur'] = nf['OLDur'].apply(lambda x: x*(1+increase))
+nf['CALCr'] = nf['CALCur'].apply(lambda x: round(x,0))
+#print of.info()
+nf.to_csv('output.csv')
